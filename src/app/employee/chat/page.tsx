089dd66875c3870fe888775c296ff10529f7dc6b
rajgr2769-web <rajgr2769@gmail.com>
2026-03-23 13:18:29 +0530

    fix suspense error

--- src/app/employee/chat/page.tsx
+++ src/app/employee/chat/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+export const dynamic = 'force-dynamic'
 import { useEffect, useMemo, useRef, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { supabase } from "@/lib/supabaseClient";
